b34cdfc15eb4ff69a8229df5dfab8300e84eba57
Bilge Han Erduran <bilgehanerduran@gmail.com>
2026-03-31 22:13:14 +0300

    initial backend setup with prisma

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const PORT = 3001
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Backend çalışıyor!' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server ${PORT} portunda çalışıyor`)
+})
