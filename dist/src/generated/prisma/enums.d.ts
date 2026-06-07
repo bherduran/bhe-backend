d41a91480439c86fe891d2eb73a9fb62e34868b8
BilgeHan <bilgehanerduran@gmail.com>
2026-06-07 15:26:53 +0300

    chore: remove dist from tracking, add to .gitignore

--- dist/src/generated/prisma/enums.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const Category: {
-    readonly web: "web";
-    readonly game: "game";
-};
-export type Category = (typeof Category)[keyof typeof Category];
-//# sourceMappingURL=enums.d.ts.map
